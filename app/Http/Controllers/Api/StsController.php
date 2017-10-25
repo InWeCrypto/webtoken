95f78ad7d27822baec409f4c5a4eba29efbfda7e
zhaoyadong <250332754@qq.com>
2017-10-25 17:57:22 +0800

    x

--- app/Http/Controllers/Api/StsController.php
+++ app/Http/Controllers/Api/StsController.php
@@ -22,15 +22,9 @@ class StsController extends BaseController
 
 // 你需要操作的资源所在的region，STS服务目前只有杭州节点可以签发Token，签发出的Token在所有Region都可用
 // 只允许子用户使用角色
-		$config = config('mq');
-		$iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", $config['Ak'], $config['Sk']);
+		$iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", env('Ak'), env('Sk'));
 		$client = new \DefaultAcsClient($iClientProfile);
-
-// 角色资源描述符，在RAM的控制台的资源详情页上可以获取
-//		$roleArn = "acs:ram::1847538160312731:role/phpserver";
-		$roleArn = "acs:ram::1847538160312731:role/phpserver";
-
-		$BUCKET_NAME = 'cryptobox';
+		$BUCKET_NAME = env('BucketName');
 		$policy = <<<POLICY
 {
   "Statement": [
@@ -54,10 +48,11 @@ POLICY;
 		$request = new AssumeRoleRequest();
 // RoleSessionName即临时身份的会话名称，用于区分不同的临时身份
 // 您可以使用您的客户的ID作为会话名称
-		$request->setRoleSessionName("phpserver");
-		$request->setRoleArn($roleArn);
+		$request->setRoleSessionName(env('RoleSessionName'));
+		// 角色资源描述符，在RAM的控制台的资源详情页上可以获取
+		$request->setRoleArn(env('RoleArn'));
 		$request->setPolicy($policy);
-		$request->setDurationSeconds(3600);
+		$request->setDurationSeconds(env('DurationSeconds'));
 		$response = $client->doAction($request);
 
 		return success(json_decode($response->getBody(), true));
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,7 @@
 
 
 Route::get('/', function () {
-
-//	dispatch(new \App\Jobs\MQChangeStatus('0x4a646e2fcc5bada1bc7e38b25e10159f228ec55d9883e787aefc059d81095f5a'));
-//	dd(\App\Model\WalletOrder::where('hash','0xac6182274dd8673dab7a3bfb3b5720c38437de9b14539d4b2102e255a3ba19b2')->get());
-//		dd('success');
-	return redirect(url('index.html'));
+	return redirect(env('APP_URL') . '/index.html');
 });
 
 
@@ -26,85 +22,86 @@ Route::get('/doc', function () {
 	echo $file;
 });
 
-function addModuleRecords(){
+function addModuleRecords()
+{
 	$modules = [
 		[
-			'title'=>'用户管理',
-			'uri'=>'',
-			'child'=>[
+			'title' => '用户管理',
+			'uri' => '',
+			'child' => [
 				[
-					'title'=>'角色管理',
-					'uri' =>'role'
+					'title' => '角色管理',
+					'uri' => 'role'
 				],
 				[
-					'title'=>'员工管理',
-					'uri' =>'admin'
+					'title' => '员工管理',
+					'uri' => 'admin'
 				],
 				[
-					'title'=>'个人信息',
-					'uri' =>'user'
-				],
-			]
-		],
-		[
-			'title'=>'Ico管理',
-			'uri'=>'',
-			'child'=>[
-				[
-					'title'=>'Ico分类',
-					'uri' =>'ico-category'
-				],
-				[
-					'title'=>'Ico列表',
-					'uri' =>'ico'
-				],
-				[
-					'title'=>'Ico订单',
-					'uri' =>'ico-order'
+					'title' => '个人信息',
+					'uri' => 'user'
 				],
 			]
 		],
 		[
-			'title'=>'钱包管理',
-			'uri'=>'',
-			'child'=>[
+			'title' => 'Ico管理',
+			'uri' => '',
+			'child' => [
 				[
-					'title'=>'钱包分类',
-					'uri' =>'wallet-category'
+					'title' => 'Ico分类',
+					'uri' => 'ico-category'
 				],
 				[
-					'title'=>'Gnt分类',
-					'uri' =>'gnt-category'
+					'title' => 'Ico列表',
+					'uri' => 'ico'
 				],
 				[
-					'title'=>'钱包列表',
-					'uri' =>'wallet'
-				],
-				[
-					'title'=>'钱包订单',
-					'uri' =>'wallet-order'
+					'title' => 'Ico订单',
+					'uri' => 'ico-order'
 				],
 			]
 		],
 		[
-			'title'=>'货币单位管理',
-			'uri'=>'monetary-unit',
+			'title' => '钱包管理',
+			'uri' => '',
+			'child' => [
+				[
+					'title' => '钱包分类',
+					'uri' => 'wallet-category'
+				],
+				[
+					'title' => 'Gnt分类',
+					'uri' => 'gnt-category'
+				],
+				[
+					'title' => '钱包列表',
+					'uri' => 'wallet'
+				],
+				[
+					'title' => '钱包订单',
+					'uri' => 'wallet-order'
+				],
+			]
 		],
 		[
-			'title'=>'文章管理',
-			'uri'=>'article',
+			'title' => '货币单位管理',
+			'uri' => 'monetary-unit',
 		],
 		[
-			'title'=>'行情分类管理',
-			'uri'=>'market-category',
+			'title' => '文章管理',
+			'uri' => 'article',
+		],
+		[
+			'title' => '行情分类管理',
+			'uri' => 'market-category',
 		]
 
 	];
-	foreach ($modules as $v){
-		$parent =\App\Model\Module::create(['name'=>$v['title'],'p_id'=>0,'uri'=>$v['uri']]);
-		if(isset($v['child'])){
+	foreach ($modules as $v) {
+		$parent = \App\Model\Module::create(['name' => $v['title'], 'p_id' => 0, 'uri' => $v['uri']]);
+		if (isset($v['child'])) {
 			foreach ($v['child'] as $val)
-				\App\Model\Module::create(['name'=>$val['title'],'p_id'=>$parent->id,'uri'=>$val['uri']]);
+				\App\Model\Module::create(['name' => $val['title'], 'p_id' => $parent->id, 'uri' => $val['uri']]);
 		}
 	}
 	return success();
