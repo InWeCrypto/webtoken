1dc685803faaeacb9a38ee70454305a195a33e11
what-00 <what-00@qq.com>
2017-11-27 17:50:12 +0800

    新增 neo 到钱包列表

--- app/Http/Controllers/Api/ConversionController.php
+++ app/Http/Controllers/Api/ConversionController.php
@@ -26,11 +26,15 @@ class ConversionController extends BaseController
 		]);
 		$wallets = json_decode($request->get('wallet_ids'), true);
 		$uri = env('API_URL',config('user_config.unichain_url'));
-		$list = Wallet::with('category')->ofUserId($this->user->id)->whereIn('id', $wallets)->get()->each(function ($val) use ($uri) {
-			$val->category->cap = Pricecoinmarketcap::ofSymbol($val->category->name)->orderBy('last_updated', 'desc')->first();
-			//钱包余额
-			$res = sendCurl($uri . '/eth/getBalance', ['address' => $val->address], null, 'POST');
-			$val->balance = $res['value'];
+		$list = Wallet::with('category')
+			->ofUserId($this->user->id)
+			->whereIn('id', $wallets)
+			->get()->each(function ($val) use ($uri) {
+				$val->category->cap = Pricecoinmarketcap::ofSymbol($val->category->name)
+										->orderBy('last_updated', 'desc')
+										->first();
+				//钱包余额
+				$val->balance = $this->getWalletBalance($val->category->name, $val->address);
 		});
 
 		return success(compact('list'));
@@ -52,5 +56,37 @@ class ConversionController extends BaseController
 		})->sortByDesc('updated_at')->values()->all();
 
 		return success(compact('record','list'));
+	}
+
+	// 钱包余额
+	public function getWalletBalance($category_name, $address){
+		$return = 0;
+		switch(strtolower($category_name)){
+			case 'eth':
+				$uri    = env('TRADER_URL_ETH', config('user_config.unichain_url')) . '/eth/getBalance';
+				$res    = sendCurl($uri, compact('address'), null, 'POST');
+				$return = $res['value'];
+			break;
+			case 'neo':
+				$neo_asset_id = \Request::header('neo-asset-id');
+				$uri          = env('TRADER_URL_NEO', config('user_config.unichain_url'));
+				$param        = [
+					'jsonrpc' => '2.0',
+					'method' => 'getaccountstate',
+					'params' => [$address],
+					'id' => 1
+				];
+				$res = sendCurl($uri, $param, null, 'POST');
+				if(!empty($res['result']['balances'])){
+					foreach($res['result']['balances'] as $val){
+						if(strcasecmp($val['asset'], $neo_asset_id) == 0){
+							$return = $val['value'];
+							break;
+						}
+					}
+				}
+			break;
+		}
+		return $return;
 	}
 }
