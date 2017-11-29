e29e616cdcd03ef78b65d7c57c37e44e4ffedb12
what-00 <what-00@qq.com>
2017-11-29 15:32:39 +0800

    钱包,错误日志

--- app/Http/Controllers/Api/WalletController.php
+++ app/Http/Controllers/Api/WalletController.php
@@ -45,6 +45,7 @@ class WalletController extends BaseController
 		try {
 			$this->checkAddress($request->get('category_id'), $request->get('address'));
 		} catch (\Exception $e) {
+			\Log::info($e->getMessage().', 请求原数据:'.json_encode($request->all()));
 			return fail('', '该钱包地址不是合法的地址');
 		}
 		$record = Wallet::create(['user_id' => $this->user->id] + $request->all());
